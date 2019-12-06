6c2d85eb8188eb38a2abf8424b31c305fbacffa0
theolamide <ajadidan@gmail.com>
2019-12-06 12:56:17 -0500

    Projects PUT and DELETE  done

--- ClientRouters/projectRouter.js
+++ ClientRouters/projectRouter.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.use(express.json());
 
 router.get('/', (req,res)=>{
+
     projects.get()
         .then(projects =>{
             res.status(200)
@@ -20,8 +21,8 @@ router.get('/', (req,res)=>{
 })
 
 router.post('/', (req,res)=>{
+    const newProject = req.body;
 
-    const newProject=req.body;
     projects.insert(newProject)
     .then(project=>{
         res.status(201)
@@ -33,6 +34,52 @@ router.post('/', (req,res)=>{
         .json({error: "There was an error while saving the project to the database" });
     });
 
+})
+
+router.put('/:id',(req,res)=>{
+    const changes = req.body;
+    const PostId = req.params.id;
+
+    if(!changes.name || !changes.description){
+        res.status(404)
+        .json({message: "Please specify changes you want made"})
+    } else {
+        projects.update(PostId,changes)
+        .then(project=>{
+            if(project){
+                res.status(200)
+                .json(project)
+            } else {
+                res.status(404)
+                .json({message: "The project with the specified ID does not exist."})
+            }
+        })
+        .catch(error=>{
+            console.log(error);
+            res.status(500)
+            .json({error: "The project information could not be modified."});
+        });
+    }
+})
+
+router.delete('/:id',(req,res)=>{
+    const toDelete = req.params.id;
+
+    projects.remove(toDelete)
+    .then(count =>{
+        if(count > 0){
+            res.status(200)
+            .json({message: "This project was succesfully deleted"})
+        } else {
+            res.status(404)
+            .json({message:'The post with the specified ID does not exist.'})
+        }
+    })
+    .catch(error=>{
+        console.log(error);
+        res.status(500)
+        .json({error:"The post could not be removed."})
+    })
 })
 
 
